4527de4344e3cc4fe19ccb37cf95262f1dd8cd57
asentaja <Asentaja@BC-5CD026D1XT.bc.local>
2022-04-28 11:30:27 +0300

    Market can now sell multiple Stocks at once. If sellAmount has value bigger than stock, error message is displayed

--- Assets/Scripts/Market.cs
+++ Assets/Scripts/Market.cs
@@ -7,44 +7,97 @@ using TMPro;
 
 public class Market : MonoBehaviour
 {
+    //needed variables for market
+    public GameObject sellError;
     public static Market instance;
     public Button sellButton;
     public Button marketButton;
     public TextMeshProUGUI stockText;
     private Factory factoryObject;
+    private int sellAmount = 1;
+    public TMP_InputField setAmount;
 
     // Start is called before the first frame update
     void Start()
     {
+        // creating instance
         if (instance == null)
         {
             instance = this;
         }
 
+        // creating a component of the factory for the market script to call stock amount
         factoryObject = GetComponent<Factory>();
 
+        // getting button components
         Button sbtn = sellButton.GetComponent<Button>();
         Button mbtn = marketButton.GetComponent<Button>();
 
+        TMP_InputField setA = setAmount.GetComponent<TMP_InputField>();
+
+        // creating button listeners
         sbtn.onClick.AddListener(SellOnClick);
         mbtn.onClick.AddListener(UpdateOnClick);
 
+        // updating stock to Stock Amount text object
         string stockNew = Convert.ToString(factoryObject.stock);
         stockText.SetText(stockNew);
+        
+        // disabling sellButton if stock less than 1
+        if (factoryObject.stock < 1)
+        {
+            sellButton.enabled = false;
+        }
     }
 
+    // objects to update when market UI is entered
     void UpdateOnClick()
     {
+        // stock update
         string stockNew = Convert.ToString(factoryObject.stock);
         stockText.SetText(stockNew);
+        
+        // enable sellButton if stock more than 0
+        if (factoryObject.stock > 0)
+        {
+            sellButton.enabled = true;
+        }
     }
 
+    // actions when sellButton is pressed
     void SellOnClick()
     {
-        factoryObject.stock = factoryObject.stock -1;
-        string stockNew = Convert.ToString(factoryObject.stock);
-        stockText.SetText(stockNew);
+        
+        if (setAmount.GetComponentInChildren<TMP_InputField>().text.Length > 0)
+        {
+            string sellAContent = setAmount.GetComponentInChildren<TMP_InputField>().text;
+            sellAmount = Convert.ToInt32(sellAContent);
+        } else
+        {
+            sellAmount = 1;
+        }
+
+        if (sellAmount <= factoryObject.stock)
+        {
+            sellError.SetActive(false);
+            // sell amount subtracted from stock and updated to screen
+            factoryObject.stock = factoryObject.stock - sellAmount;
+            string stockNew = Convert.ToString(factoryObject.stock);
+            stockText.SetText(stockNew);
+        } else
+        {
+            sellError.SetActive(true);
+        }
+        
+
+        // if stock below 1 disable sell button
+        if (factoryObject.stock < 1)
+        {
+            sellButton.enabled = false;
+        }
     }
+
+    
 
     // Update is called once per frame
     void Update()
